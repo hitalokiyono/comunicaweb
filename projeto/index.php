bf4a6831a40d3878b1b1df4f547ee95ba8b469a0
hitalokiyono <144763553+hitalokiyono@users.noreply.github.com>
2024-08-07 20:55:48 -0300

    Auto-commit

--- projeto/index.php
+++ projeto/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>comunica</title>
+    <title>comunicaweb</title>
 </head>
 <body>
     
