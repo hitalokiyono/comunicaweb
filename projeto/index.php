31ccfc3bd203eb81daf488700c405957121ad963
hitalokiyono <144763553+hitalokiyono@users.noreply.github.com>
2024-08-08 00:21:27 -0300

    Auto-commit

--- projeto/index.php
+++ projeto/index.php
@@ -2,7 +2,7 @@
 <html lang="pt">
 <head>
     <meta charset="UTF-8">
-    
+    <?php require_once ("./menu_principal.php"); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/estilos.css">
     <title>comunicaweb</title>
