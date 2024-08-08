af05fbefbbbd389489055bd4916027a54a16e329
hitalokiyono <144763553+hitalokiyono@users.noreply.github.com>
2024-08-08 00:16:55 -0300

    Auto-commit

--- projeto/index.php
+++ projeto/index.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once ("./menu_principal.php"); ?>
     <link rel="stylesheet" href="./css/estilos.css">
     <title>comunicaweb</title>
 </head>
 <body>
-<?php require_once ("./menu_principal.php"); ?>
+
 <div  class="containermain">
 <video autoplay muted loop id="video">
     <source src="./midia/videos/fundoprincipal.mp4" type="video/mp4">
