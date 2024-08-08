7c385db10cef24daa9b2808b2e3cc03a69cf21b9
hitalokiyono <144763553+hitalokiyono@users.noreply.github.com>
2024-08-08 00:14:44 -0300

    Auto-commit

--- projeto/index.php
+++ projeto/index.php
@@ -12,7 +12,6 @@
 <video autoplay muted loop id="video">
     <source src="./midia/videos/fundoprincipal.mp4" type="video/mp4">
   </video>
-  <?php require_once ("./menu_principal.php"); ?>
   </div>
 </body>
 </html>
