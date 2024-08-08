883fa74cdbbbf626db61bd3820a5854b75f344ee
hitalokiyono <144763553+hitalokiyono@users.noreply.github.com>
2024-08-07 23:41:55 -0300

    Auto-commit

--- projeto/menu_principal.php
+++ projeto/menu_principal.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>jogo</title>
+    
     <link rel="stylesheet" href="./css/menu_principal.css">
 </head>
 <body>
