93edfe6c19d198bc808d6049d1768f77cdf269b5
hitalokiyono <144763553+hitalokiyono@users.noreply.github.com>
2024-08-08 00:04:08 -0300

    Auto-commit

--- projeto/index.php
+++ projeto/index.php
@@ -7,6 +7,8 @@
     <title>comunicaweb</title>
 </head>
 <body>
-    
+<video autoplay muted loop id="meuVideo">
+    <source src="../videos/champ.mp4" type="video/mp4">
+  </video>
 </body>
 </html>
