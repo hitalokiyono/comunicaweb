157e67c3f1b8b7b6025bc6e7e4de881445e81cd4
hitalokiyono <144763553+hitalokiyono@users.noreply.github.com>
2024-08-09 11:53:50 -0300

    Auto-commit

--- projeto/index.php
+++ projeto/index.php
@@ -11,7 +11,7 @@
 
 <div class = "main">
  <div class="login"  onclick="login">
-    <h3> REGISTRAR-SE:</h3>
+    <h3> REGISTRAR-SE</h3>
   <div class="container">
     <form>
       <div class="form-group">
