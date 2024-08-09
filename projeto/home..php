616e02d53af7d60347edfcb3169ed2e1d24ee597
hitalokiyono <144763553+hitalokiyono@users.noreply.github.com>
2024-08-09 11:57:44 -0300

    Auto-commit

--- projeto/home..php
+++ projeto/home..php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <?php require_once ("./menu_principal.php"); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/e.css">
+    <link rel="stylesheet" href="./css/home.css">
     <title>comunicaweb</title>
 </head>
 <body>
